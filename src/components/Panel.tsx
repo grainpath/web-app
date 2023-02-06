4cac5986c470fb69dbba1058759665a14d8a891f
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-02-05 20:34:23 -0500

    implement panel header

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
 import Offcanvas from 'react-bootstrap/Offcanvas';
-import { EraseButton, ListButton } from './Button';
+import { PanelView } from '../utils/types';
+import { erase } from '../features/searchSlice';
+import { setRemote } from '../features/panelsSlice';
+import { useAppDispatch, useAppSelector } from '../features/hooks';
+import { RemoteButton, EraseButton } from './Button';
 
 type PanelProps = {
   visibility: boolean;
   onHide: React.MouseEventHandler<HTMLButtonElement>;
 }
 
+type SearchHeaderProps = {
+  erase: React.MouseEventHandler<HTMLButtonElement>;
+  remote: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+function SearchHeader({ erase, remote }: SearchHeaderProps): JSX.Element {
+
+  return (
+    <>
+      <RemoteButton onClick={remote} />
+      <EraseButton onClick={erase} />
+    </>
+  );
+}
+
 export function Panel({ visibility, onHide }: PanelProps) {
+
+  const dispatch = useAppDispatch();
+  const panel = useAppSelector(state => state.panels.value);
 
   return(
     <Offcanvas show={visibility} onHide={onHide} scroll={true} backdrop={false} keyboard={false}>
       <Offcanvas.Header closeButton>
-        <EraseButton onClick={() => {}} />
-        <ListButton onClick={() => {}} />
+        {panel === PanelView.Search && <SearchHeader erase={() => { dispatch(erase()) }} remote={() => { dispatch(setRemote()) }} />}
+        {panel === PanelView.Result && <></>}
+        {panel === PanelView.Result && <></>}
       </Offcanvas.Header>
     </Offcanvas>
   );
