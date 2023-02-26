10f2cb4fcf0ed0febac5ec53e4267317ccbc9d6f
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-02-25 19:19:12 -0500

    refactor count input

--- src/components/Search/CountInput.tsx
+++ src/components/Search/CountInput.tsx
@@ -1,9 +1,12 @@
-import { useEffect, useRef, useState } from 'react';
-import { Form, InputGroup } from 'react-bootstrap';
-import { useAppDispatch, useAppSelector } from '../../features/hooks';
-import { setCount } from '../../features/searchSlice';
+import { useEffect, useRef, useState } from "react";
+import { Form, InputGroup } from "react-bootstrap";
+import { setCount } from "../../features/searchSlice";
+import { useAppDispatch, useAppSelector } from "../../features/hooks";
 
 export function CountInput(): JSX.Element {
+
+  const defaultInput = "";
+  const minVal = 1, maxVal = 1000;
 
   const [valid, setValid] = useState(true);
   const inputRef = useRef<HTMLInputElement>(null);
@@ -16,23 +19,22 @@ export function CountInput(): JSX.Element {
   }, [count]);
 
   const checkIn = (input: string) => {
-
     var c = parseInt(input);
-    var s = input !== "" && c >= 0 && c <= 1000;
+    var s = input !== defaultInput && c >= minVal && c <= maxVal;
 
     setValid(s);
     if (s) { dispatch(setCount(c)); }
   };
 
   return (
-    <Form.Group className='mt-4 mb-4'>
+    <Form.Group className="mt-4 mb-4">
       <Form.Label>Count</Form.Label>
       <InputGroup>
-        <Form.Control ref={inputRef} type='number' defaultValue={count} min={1} max={1000}
-                      step={1} isInvalid={!valid} onChange={(e) => { checkIn(e.target.value); }} />
+        <Form.Control ref={inputRef} type="number" defaultValue={count} min={minVal} max={maxVal}
+          step={1} isInvalid={!valid} onChange={(e) => { checkIn(e.target.value); }} />
         <InputGroup.Text>pts</InputGroup.Text>
       </InputGroup>
-      <Form.Text>integer between 0 and 1000</Form.Text>
+      <Form.Text>integer between {minVal} and {maxVal}</Form.Text>
     </Form.Group>
   );
 }
