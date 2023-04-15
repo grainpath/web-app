2be06e4435073a3f9184de5f927a7e2874297085
Dmitry Zhukov <zhukov.dm.s@gmail.com>
2023-04-15 06:56:56 -0400

    add solid wrapper

---
+++ src/utils/solidStorage.ts
@@ -0,0 +1,64 @@
+import { IStorage } from "../domain/interfaces";
+import { StoredPlace, StoredRoute, StoredDirection } from "../domain/types";
+
+/**
+ * Wrapper over decentralized Solid Pod.
+ */
+export class SolidStorage implements IStorage {
+
+  // [C]reate
+
+  createPlace(place: StoredPlace): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  createRoute(route: StoredRoute): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  createDirection(direction: StoredDirection): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  // [R]ead
+
+  getAllPlaces(): Promise<StoredPlace[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  getAllRoutes(): Promise<StoredRoute[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  getAllDirections(): Promise<StoredDirection[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  // [U]pdate
+
+  updatePlace(place: StoredPlace): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  updateRoute(route: StoredRoute): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  updateDirection(direction: StoredDirection): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  // [D]elete
+
+  deletePlace(placeId: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  deleteRoute(routeId: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  deleteDirection(directionId: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+}
